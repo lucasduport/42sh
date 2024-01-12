45d1a451588f53128c3e165e21168953ba981ecd
Mathilde Copin <mathilde.copin@epita.fr>
2024-01-12 12:22:31 +0100

    fix(lexer): edit way to print token

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -20,38 +20,38 @@
 */
 static void print_token(struct token token)
 {
-    debug_printf("[LEX] Data: %s", token.data);
+    debug_printf("[TOKEN] Data: %s", token.data);
     switch (token.type)
     {
     case TOKEN_IF:
-        debug_printf("[LEX] Type: TOKEN_IF");
+        debug_printf("[TOKEN] Type: TOKEN_IF");
         break;
     case TOKEN_THEN:
-        debug_printf("[LEX] Type: TOKEN_THEN");
+        debug_printf("[TOKEN] Type: TOKEN_THEN");
         break;
     case TOKEN_ELIF:
-        debug_printf("[LEX] Type: TOKEN_ELIF");
+        debug_printf("[TOKEN] Type: TOKEN_ELIF");
         break;
     case TOKEN_ELSE:
-        debug_printf("[LEX] Type: TOKEN_ELSE");
+        debug_printf("[TOKEN] Type: TOKEN_ELSE");
         break;
     case TOKEN_FI:
-        debug_printf("[LEX] Type: TOKEN_FI");
+        debug_printf("[TOKEN] Type: TOKEN_FI");
         break;
     case TOKEN_SEMICOLONS:
-        debug_printf("[LEX] Type: TOKEN_SEMICOLONS");
+        debug_printf("[TOKEN] Type: TOKEN_SEMICOLONS");
         break;
     case TOKEN_NEWLINE:
-        debug_printf("[LEX] Type: TOKEN_NEWLINE");
+        debug_printf("[TOKEN] Type: TOKEN_NEWLINE");
         break;
     case TOKEN_WORD:
-        debug_printf("[LEX] Type: TOKEN_WORD");
+        debug_printf("[TOKEN] Type: TOKEN_WORD");
         break;
     case TOKEN_EOF:
-        debug_printf("[LEX] Type: TOKEN_EOF");
+        debug_printf("[TOKEN] Type: TOKEN_EOF");
         break;
     default:
-        debug_printf("[LEX] Type: TOKEN_UNKNOWN");
+        debug_printf("[TOKEN] Type: TOKEN_UNKNOWN");
         break;
     }
 }
