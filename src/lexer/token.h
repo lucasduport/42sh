52940f89d7524bb97f19cee1fcbba1ba2c7246b8
Flavien Geoffray <flavien.geoffray@epita.fr>
2024-01-16 16:30:59 +0100

    feat(lexer): add new tokens

--- src/lexer/token.h
+++ src/lexer/token.h
@@ -5,13 +5,41 @@
 
 enum token_type
 {
+    // -------- RESERVED WORDS --------
     TOKEN_IF = 0,
     TOKEN_THEN,
     TOKEN_ELIF,
     TOKEN_ELSE,
     TOKEN_FI,
-    TOKEN_SEMICOLONS,
-    TOKEN_NEWLINE,
+    TOKEN_DO,
+    TOKEN_DONE,
+
+    TOKEN_WHILE,
+    TOKEN_UNTIL,
+    TOKEN_FOR,
+    TOKEN_IN,
+    TOKEN_NEG,
+
+    // ------- CONTROL OPERATORS -------
+    TOKEN_SEMICOLONS, // ;
+    TOKEN_NEWLINE, // \n
+    TOKEN_PIPE, // |
+    TOKEN_AND_IF, // &&
+    TOKEN_OR_IF, // ||
+    TOKEN_DSEMI, // ;;
+
+    // ---- REDIRECTION OPERATORS ------
+    TOKEN_LESS, // <
+    TOKEN_GREAT, // >
+    TOKEN_DLESS, // <<
+    TOKEN_DGREAT, // >>
+    TOKEN_LESSAND, // <&
+    TOKEN_GREATAND, // >&
+    TOKEN_LESSGREAT, // <>
+    TOKEN_DLESSDASH, // <<-
+    TOKEN_CLOBBER, // >|
+
+    // ---------------------------------
     TOKEN_EOF,
 
     TOKEN_WORD,
@@ -19,9 +47,20 @@ enum token_type
     TOKEN_NULL,
 };
 
+enum token_family
+{
+    TOKEN_FAM_RESERVED,
+    TOKEN_FAM_OPERATOR,
+    TOKEN_FAM_REDIR,
+    TOKEN_FAM_IO_NUMBER,
+    TOKEN_FAM_WORD,
+    //TOKEN_FAM_ASSIGNMENT_W,
+};
+
 struct token
 {
     enum token_type type;
+    enum token_family family;
     char *data;
 };
 
