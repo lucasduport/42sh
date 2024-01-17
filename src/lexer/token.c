303caab57c5ea72f3244a2f25469d1d4aedb278b
Lucas Duport <lucas.duport@epita.fr>
2024-01-17 22:36:10 +0100

    style: clang tidy

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -4,25 +4,21 @@
 
 void print_token(struct token token)
 {
-    char *token_fam[] =
-    {
-        "RESERVED", "OPERATOR", "REDIR", "IO_NUMBER",
-        "ASSIGNMENT_W", "WORD"
-    };
- 
-    char *token_type[] =
-    {
-        "IF", "THEN", "ELIF", "ELSE", "FI", "DO", "DONE",
-        "WHILE", "UNTIL", "FOR", "IN", "NEG", "SEMICOLONS",
-        "NEWLINE", "PIPE", "AND_IF", "OR_IF", "DSEMI",
-        "LESS", "GREAT", "DLESS", "DGREAT", "LESSAND", "GREATAND",
-        "LESSGREAT", "DLESSDASH", "CLOBBER", "EOF", "AND", "OR",
-        "WORD", "NULL" 
-    };
+    char *token_fam[] = { "RESERVED",  "OPERATOR",     "REDIR",
+                          "IO_NUMBER", "ASSIGNMENT_W", "WORD" };
+
+    char *token_type[] = { "IF",         "THEN",      "ELIF",    "ELSE",
+                           "FI",         "DO",        "DONE",    "WHILE",
+                           "UNTIL",      "FOR",       "IN",      "NEG",
+                           "SEMICOLONS", "NEWLINE",   "PIPE",    "AND_IF",
+                           "OR_IF",      "DSEMI",     "LESS",    "GREAT",
+                           "DLESS",      "DGREAT",    "LESSAND", "GREATAND",
+                           "LESSGREAT",  "DLESSDASH", "CLOBBER", "EOF",
+                           "AND",        "OR",        "WORD",    "NULL" };
 
     debug_printf(LOG_LEX,
-        "[TOKEN] Family: TOKEN_FAM_%s | Type: TOKEN_%s | Data: %s\n",
-        token_fam[token.family], token_type[token.type], token.data); 
+                 "[TOKEN] Family: TOKEN_FAM_%s | Type: TOKEN_%s | Data: %s\n",
+                 token_fam[token.family], token_type[token.type], token.data);
 }
 
 struct token token_null(void)
