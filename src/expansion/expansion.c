5f72e48587d0d1429fb0666c952c05878ef682c8
Lucas Duport <lucas.duport@epita.fr>
2024-01-17 12:39:42 +0100

    fix: single quote expansion

--- src/expansion/expansion.c
+++ src/expansion/expansion.c
@@ -11,84 +11,47 @@ int expansion(struct list *arguments, struct environment *env)
     struct list *arg = arguments;
     while (arg != NULL)
     {
-        if (arg->current[0] == '\'')
+        char *current = arg->current;
+        while (current != '\0')
         {
-            if (expand_single_quotes(&arg->current, env) == -1)
+            char *next = NULL;
+
+            if (current == '\'')
+                next = expand_single_quotes(&current, env);
+            else if (current == '\"')
+                next = expand_double_quotes(&current, env);
+            else if (current == '$')
+                next = expand_dollar(&current, env);
+
+            // Check if an error occured
+            if (next == NULL)
                 return -1;
-        }
-        else if (arg->current[0] == '\"')
-        {
-            if (expand_double_quotes(&arg->current, env) == -1)
-                return -1;
-        }
-        else
-        {
-            if (expand_simple(&arg->current, env) == -1)
-                return -1;
+            current = next;
         }
     }
     return 0;
 }
 
 /**
- * @brief Expand single quotes: replace any \n or ... by their respective
- * characters
+ * @brief Expand single quotes: does not expand anything
  *
  * @param str String to expand
  * @param env Environment
- * @return int 0 on success, -1 on error
+ * @return char * index of the character after the last quote, NULL if no
  */
-static int expand_single_quotes(char **str, struct environment *env)
+static char *expand_single_quotes(char **str, struct environment *env)
 {
-    for (int i = 0; (*str)[i] != '\0'; i++)
+    char *first_quote = *str;
+    for (int i = 1; (*str)[i] != '\0'; i++)
     {
-        if ((*str)[i] == '\\')
+        if ((*str)[i] == '\'')
         {
-            switch ((*str)[i + 1])
-            {
-            case 'n':
-                (*str)[i] = '\n';
-                break;
-            case 't':
-                (*str)[i] = '\t';
-                break;
-            case 'v':
-                (*str)[i] = '\v';
-                break;
-            case 'b':
-                (*str)[i] = '\b';
-                break;
-            case 'r':
-                (*str)[i] = '\r';
-                break;
-            case 'f':
-                (*str)[i] = '\f';
-                break;
-            case 'a':
-                (*str)[i] = '\a';
-                break;
-            case '\\':
-                (*str)[i] = '\\';
-                break;
-            case '\'':
-                (*str)[i] = '\'';
-                break;
-            case '\"':
-                (*str)[i] = '\"';
-                break;
-            case '\?':
-                (*str)[i] = '\?';
-                break;
-            default:
-                debug_printf(LOG_EXPANSION,
-                             "[EXPANSION] Unknown escape sequence: \\%c\n",
-                             (*str)[i + 1]);
-                return -1;
-            }
-            for (int j = i + 1; (*str)[j] != '\0'; j++)
-                (*str)[j] = (*str)[j + 1];
+            remove_at_n(str, first_quote);
+            remove_at_n(str, i - 1);
+            return *str + i - 1;
         }
     }
+    return NULL;
 }
 
 static int expand_double_quotes(char **str, struct environment *env)
@@ -96,7 +59,52 @@ static int expand_double_quotes(char **str, struct environment *env)
     // TODO: implement
 }
 
-static int expand_simple(char **str, struct environment *env)
+static int expand_dollar(char **str, struct environment *env)
 {
     // TODO: implement
+}
+
+/**
+ * @brief Expand a variable
+ *
+ * @param str String to expand
+ * @param to_insert String to insert
+ * @param n Index where to insert
+ */
+static void insert_at_n(char **str, char *to_insert, int n)
+{
+    char *new_str = calloc(strlen(*str) + strlen(to_insert) + 1, sizeof(char));
+    if (new_str == NULL)
+    {
+        debug_printf(LOG_EXP,
+                     "[EXPANSION] insert_at_n: new_str allocation failed\n");
+        return;
+    }
+    strncpy(new_str, *str, n);
+    strcat(new_str, to_insert);
+    strcat(new_str, *str + n);
+    free(*str);
+    *str = new_str;
+}
+
+/**
+ * @brief Remove a character at a given index. The string is reallocated.
+ *
+ *
+ * @param str String to remove from
+ * @param n Index to remove
+ */
+static void remove_at_n(char **str, int n)
+{
+    char *new_str = calloc(strlen(*str) + 1, sizeof(char));
+    if (new_str == NULL)
+    {
+        debug_printf(LOG_EXP,
+                     "[EXPANSION] remove_at_n: new_str allocation failed\n");
+        return;
+    }
+    strncpy(new_str, *str, n);
+    strcat(new_str, *str + n + 1);
+    free(*str);
+    *str = new_str;
 }
