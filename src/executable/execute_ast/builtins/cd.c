34b79a82eb0dc83adffa7527524f0897464eedc8
Corentin Belloni <corentin.belloni@epita.fr>
2024-01-23 22:33:31 +0100

    feat(builtin): implement cd builtin

--- src/executable/execute_ast/builtins/cd.c
+++ src/executable/execute_ast/builtins/cd.c
@@ -1,36 +1,8 @@
-#define _POSIX_C_SOURCE 200112L
+#define _POSIX_C_SOURCE 200809L
 
 #include "builtins.h"
 
 #include <stdlib.h>
-
-static int is_dot(char *path)
-{
-    size_t i = 0;
-    char *dot = ".";
-
-    while (path[i] != '\0' && path[i] != '/')
-    {
-        if (path[i] != dot[i])
-            return 0; 
-    }
-
-    return 1;
-}
-
-static int is_dotdot(char *path)
-{    
-    size_t i = 0;
-    char *dot = "..";
-
-    while (path[i] != '\0' && path[i] != '/')
-    {
-        if (path[i] != dot[i])
-            return 0; 
-    }
-
-    return 1;
-}
 
 int builtin_cd(struct list *list)
 {
@@ -39,22 +11,35 @@ int builtin_cd(struct list *list)
 
     // rule 1
     if (list->next == NULL && getenv("HOME") == NULL)
-        return -1;
+        return 1;
     
     // rule 2
     if (list->next == NULL)
-        list_append(&list, getenv("HOME"));
+        list_append(&list, strdup(getenv("HOME")));
 
     list = list->next;
+    currpath = list->current;
 
-    // rule 3 - 6
-    currpath = list->current;  
+    // cd -    
+    if (!strcmp(list->current, "-"))
+    {
+        currpath = getenv("OLDPWD");
+        printf("%s\n", currpath);
+    }
+    if (list->current[0] == '~')
+        currpath = getenv("HOME");
 
-    // rule 8
-    // TODO
+    if (chdir(currpath) == -1)
+    {
+        fprintf(stderr, "cd: no such file or directory: %s\n", currpath);
+        return 1;
+    }
 
-    chdir(currpath);
-    setenv("OLDPWD", currpath, 1);
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+
+    setenv("PWD", cwd, 1);
+    setenv("OLDPWD", pwd, 1);
 
     return 0;
 }
